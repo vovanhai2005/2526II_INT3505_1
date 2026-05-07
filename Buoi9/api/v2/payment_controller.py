42d9bcf8906569a7e4164678efda3ded240e2d69
vovanhai2005 <vohai10112005@gmail.com>
2026-05-08 00:03:55 +0700

    fix(update): update v2 payment controller

--- Buoi9/api/v2/payment_controller.py
+++ Buoi9/api/v2/payment_controller.py
@@ -1,3 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from services.payment_service import process_payment, payment_response
 
 api_v2 = Blueprint('api_v2', __name__)
+
+@api_v2.route('/payments', methods=['POST'])
+def create_payment_v2():
+    """
+    Tạo giao dịch thanh toán đa tiền tệ
+    """
+    data = request.get_json()
+    
+    if not data or 'amount' not in data or 'currency' not in data:
+        error_response = payment_response(
+            status="error", 
+            message="Thieu du lieu!"
+        )
+        return jsonify(error_response), 400
+
+    amount = data['amount']
+    currency = str(data['currency']).upper()
+    
+    allowed_currencies = ["VND", "USD", "EUR", "JPY"]
+    if currency not in allowed_currencies:
+        error_response = payment_response(
+            status="error", 
+            message=f"{currency} chua duoc ho tro!"
+        )
+        return jsonify(error_response), 400
+        
+    result_dict, status_code = process_payment(amount, currency)
+
+    return jsonify(result_dict), status_code
